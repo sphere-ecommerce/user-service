f3ae277bb20cfa43b8e1432badac085be580ae03
Ankit kumar <ankit.kumar.info22@gmail.com>
2025-04-25 08:07:13 +0530

    update controller

--- src/main/java/com/ecommerce/user_service/controller/AuthController.java
+++ src/main/java/com/ecommerce/user_service/controller/AuthController.java
@@ -46,17 +46,5 @@ public class AuthController {
         }
     }
 
-    @DeleteMapping("/delete")
-    public ResponseEntity<ApiResponse<String>> deleteUser(@RequestHeader("Authorization") String token) {
-        try {
-            log.info("Deleting user with token: {}", token);
-            String email = jwtUtil.extractEmail(token.replace("Bearer ", ""));
-            userService.deleteByEmail(email);
-            return ResponseEntity.ok(new ApiResponse<>("User deleted successfully", null));
-        } catch (Exception e) {
-            log.error("Error during user deletion: {}", e.getMessage());
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body(new ApiResponse<String>("User deletion failed", e.getMessage()));
-        }
-    }
+
 }
